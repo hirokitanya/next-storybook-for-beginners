ebd15b629e9d38e52564a78097a22cc6fa1a6bb7
hirokitanya <hiroki.tanya@fox-hound.jp>
2024-07-05 18:06:49 +0900

    ダッシュボードのstorybookを作成

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -1,24 +1,23 @@
-import { ChartData, DepositData, OrderData } from './types';
-import Order from './order';
-import Deposit from './deposit';
 import Chart from './chart';
+import Deposit from './deposit';
+import Order from './order';
+import { ChartData, DepositData, OrderData } from './types';
+
 export default function Dashboard({
   chart,
   deposit,
   order,
 }: Readonly<{ chart: ChartData; deposit: DepositData; order: OrderData }>) {
-  // propsとして渡す
-  console.log(chart, deposit, order);
   return (
     <div className="row-span-2 grid grid-cols-4">
-      <span className="col-span-3 mr-3">
+      <div className="col-span-3 mr-3">
         {/* chart  */}
         <Chart data={chart.data} />
-      </span>
-      <span className="col-span-1 ml-3">
+      </div>
+      <div className="col-span-1 ml-3">
         {/* deposit */}
         <Deposit data={deposit.data} />
-      </span>
+      </div>
       <div className="grit-rows-5 col-span-4 my-8">
         <Order data={order.data} />
       </div>
